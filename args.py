f57de972731909b9e4f167927c7baa4f970a6e92
chandrakiran-1 <gudlachandrakiran@gmail.com>
2026-05-12 14:45:21 +0530

    args is updated and added kwargs

--- args.py
+++ args.py
@@ -1,3 +1,4 @@
+# args is used to when the no . of arguements are unknown.
 def total(*args):
     return sum(args)
 print(total(1,2,3,4))
---
+++ kwargs.py
@@ -0,0 +1,3 @@
+def info(**kwargs):
+    print(kwargs)
+info(name = "chandu" , age = 21)
