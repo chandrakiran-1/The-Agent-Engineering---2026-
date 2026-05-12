cfbdf588bb1e8c3e0ea968727db04e8d166f0387
chandrakiran-1 <gudlachandrakiran@gmail.com>
2026-05-12 21:36:36 +0530

    list comphersions

--- decorators_with_arguments.py
+++ decorators_with_arguments.py
@@ -1,16 +1,15 @@
 def decorators(func):
-    def wrapper(*args , **kwargs):
-        print("before function:")
-        func(*args , **kwargs)
+    def wrapper(*args, **kwargs):
+        print("Before function:")
+        func(*args,**kwargs)
         print("After function:")
     return wrapper
+@decorators
+def wish(name):
+    print(f"hello {name}")
+wish("chandu")
 
 @decorators
-def greet(name):
-    print(f"Hello, {name}")
-greet("chandu")
-
-@decorators
-def user(age , rollno):
-    print( "age : ",age ,"rollno : ",rollno)
-user(age=20 , rollno=11)
+def info(age , rollno):
+    print("age :",age , "rollno :",rollno)
+info(age=21, rollno=11)
---
+++ list_comphersions.py
@@ -0,0 +1,14 @@
+
+even = [i for i  in range(100) if i%2 ==0]
+print(even)
+
+odd =  [ i for i in range(100) if i%2 !=0]
+print(odd)
+
+n = [ i for i in range(100)]
+print(n)
+
+names = ["chandu","sam","ram"]
+up = [name.upper() for name in names]
+print(up)
+
