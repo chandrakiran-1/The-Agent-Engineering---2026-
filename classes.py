0079846b9b55db679c1961cd9a7eb2994be6a8e1
chandrakiran-1 <gudlachandrakiran@gmail.com>
2026-05-14 00:32:02 +0530

    updated

--- classes.py
+++ classes.py
@@ -1,23 +1,39 @@
 class Employee:
+
+    raised_percentage= 1.04 # class variable
+    count = 0
+
+    __slots__ = ["frist" , "last" , "pay" ,"email"]
+
     def __init__(self , frist , last , pay):
         self.frist = frist
         self.last = last
         self.pay = pay
         self.email = f"{frist.lower()}.{last.lower()}@infosys.com"
+        Employee.count+= 1
+        
     def Full_name(self):
         return f"{self.frist} {self.last}"
-    def Increase_pay(self , percent):
-        self.pay = int(self.pay * (1 + percent/100))
+    def Increase_pay(self):
+        self.pay = int(self.pay * self.raised_percentage)
 
 emp1 = Employee("Chandu", "reddy" , 60000)
 emp2 = Employee("Sharma", "kapoor", 50000)
 
 
-print(emp1.__dict__)
-print(emp2.__dict__)
-print(emp1.pay)
-emp1.Increase_pay(5)
+
+
+
 print(emp1.pay)
 
-Employee.Increase_pay(emp1 , 7)
+print(Employee.raised_percentage)
+
+print(emp1.raised_percentage)
+print(emp2.raised_percentage)
+emp1.Increase_pay()
 print(emp1.pay)
+
+print(emp1.raised_percentage)
+print(Employee.__dict__)
+print(Employee.count)
+print(emp1.count)
