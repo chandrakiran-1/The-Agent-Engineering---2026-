c69b75f7c8eb0c2939b58c64e6e3cff6cf53c566
chandrakiran-1 <gudlachandrakiran@gmail.com>
2026-05-17 07:44:11 +0530

    inheritance ex

--- inheritance.py
+++ inheritance.py
@@ -14,3 +14,13 @@ class Dog(Animal):
     pass
 d = Dog()
 d.animal()
+
+class Vehicle:
+    def Start(self):
+        print("the car is Started:")
+class Car(Vehicle):
+    def Drive(self):
+        print("the car is driving by  her:")
+    pass
+car = Car()
+car.Start()
