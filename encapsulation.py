4f6ec59890d73d2b114575f6830e8ddcbca11308
chandrakiran-1 <gudlachandrakiran@gmail.com>
2026-05-18 00:34:52 +0530

    encapsulation and dunder methods:

--- encapsulation.py
+++ encapsulation.py
@@ -8,3 +8,24 @@ class Bank:
 b = Bank()
 b.deposit(1200)
 b.show_balance()
+
+class Demo:
+    def __len__(self):
+        return 6
+d = Demo()
+print(len(d))
+
+class String:
+    def __str__(self):
+        return " this is a sentence:"
+s = String()
+print(s)
+
+class Constructor:
+    def __init__(self , name):
+        self.name = name
+    def display(self):
+        return self.name
+c  = Constructor("chandu")
+res = c.display()
+print(res)
