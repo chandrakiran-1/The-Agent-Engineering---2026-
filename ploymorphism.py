bb1f3b311ab9b1c6bab902f3fe3a21c21c646b0f
chandrakiran-1 <gudlachandrakiran@gmail.com>
2026-05-18 00:03:45 +0530

    ploymorphism :

---
+++ ploymorphism.py
@@ -0,0 +1,10 @@
+class Vehicle:
+    def start(self):
+        print("vehicle is starting:")
+class Car:
+    def start(self):
+        print("car is starting:")
+v = Vehicle()
+c = Car()
+v.start()
+c.start()
