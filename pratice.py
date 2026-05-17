43379c6d4aa1a761541659742a17e2c977819b67
chandrakiran-1 <gudlachandrakiran@gmail.com>
2026-05-17 07:31:32 +0530

    pratice -1

---
+++ pratice.py
@@ -0,0 +1,59 @@
+class Calculator:
+    @staticmethod
+    def multiply(a,b):
+        return a*b
+print(Calculator.multiply(2,3))
+
+
+class Student:
+    def __init__(self ,name):
+        self.name = name
+    def show(self):
+        print("name : ", self.name)
+std = Student("chandu")
+std.show()
+
+class Employee:
+    company = "google"
+    def __init__(self, name):
+        self.name = name 
+    @classmethod
+    def Change_company(cls , new_company):
+        cls.company = new_company
+print(Employee.company)
+Employee.Change_company("microsoft")
+print(Employee.company)
+
+class Math:
+    @staticmethod
+    def add(a,b):
+        return a+b
+print(Math.add(2,4))
+
+class Math1:
+    def __init__(self, a,b):
+        self.a =a
+        self.b =b
+    def sub(self):
+        return self.a-self.b
+Sub = Math1(2,3)
+result = Sub.sub()
+print(result)
+
+class BankAccount:
+    def __init__(self , name , balance):
+        self.name = name
+        self.balance = balance
+    def deposit(self , deposit):
+        self.balance = self.balance + deposit
+    def withdraw(self , amount):
+        if amount > self.balance:
+            print("Invalid transcation:")
+        else:
+            self.balance = self.balance - amount
+    def show_balance(self):
+        print("current balance is : ", self.balance)
+acc = BankAccount("chandu",40000)
+acc.deposit(30000)
+acc.withdraw(10000)
+acc.show_balance()
