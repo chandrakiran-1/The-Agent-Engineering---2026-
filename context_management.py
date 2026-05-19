386e4d0371852ef78e9c53462c95f41963ccbcdb
chandrakiran-1 <gudlachandrakiran@gmail.com>
2026-05-19 17:33:52 +0530

    context - management

---
+++ context_management.py
@@ -0,0 +1,11 @@
+with open("demo.txt","w") as f:
+    f.write("hello all welcome to agentic ai ")
+    
+
+
+
+    # with keyword is used to close the file automatically without using the close()
+
+    file = open("demo.txt" , "w")
+    file.write("promise 2026 ending we will land in a agentic engineer")
+    file.close
