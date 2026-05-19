7ec84eb24e362d855d13e1610b0eaa25af88f639
chandrakiran-1 <gudlachandrakiran@gmail.com>
2026-05-19 15:31:12 +0530

    Counter

---
+++ Counters.py
@@ -0,0 +1,9 @@
+# counter is used to find the count of the each and every elements without any loop.
+
+from collections import Counter
+a = ['a','a','a','b','c','c','b']
+b = Counter(a)
+print(b)
+x = "chandrakiran"
+y =Counter(x)
+print(y)
