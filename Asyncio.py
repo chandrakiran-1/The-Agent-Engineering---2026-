e4a5b8ec881d6219ca7bf5c4d29c9042240de687
chandrakiran-1 <gudlachandrakiran@gmail.com>
2026-07-05 20:32:54 +0530

    updated the asyncio:

--- Asyncio.py
+++ Asyncio.py
@@ -5,3 +5,13 @@ async def wish():
     await asyncio.sleep(3)
     print("2")
 asyncio.run(wish())
+
+async def num():
+    print("one")
+    await asyncio.sleep(2)
+    print("two")
+    await asyncio.sleep(3)
+    print("Three")
+    await asyncio.sleep(4)
+    print("Four")
+asyncio.run(num())
